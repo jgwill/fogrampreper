dc53446b76eaf529fa12edf3a807d8cbd88ad361
Guillaume Isabelle <jgi@jgwill.com>
2019-06-11 12:28:54 -0400

    not reading the Nikon JPG or NEF

--- foexif_read_exit_guess_hdr.js
+++ foexif_read_exit_guess_hdr.js
@@ -1,3 +1,19 @@
 //@STCGoal A sequence of shooting contains many HDR.  We read the Exif, observe the BKT -0.7, -1.0 or other data to learn which are sequence
 //@a We run luminance-hdr-cli -g 1.1 -s ../my_c.tif DSC_1273.NEF  DSC_1274.NEF  DSC_1275.NEF  DSC_1276.NEF  DSC_1277.NEF
 
+//
+
+
+var ExifImage = require('exif').ExifImage;
+
+
+try {
+  new ExifImage({ image : './samples/DSC_1280.jpg' }, function (error, exifData) {
+      if (error)
+          console.log('Error: '+error.message);
+      else
+          console.log(exifData); // Do something with your data!
+  });
+} catch (error) {
+  console.log('Error: ' + error.message);
+}
