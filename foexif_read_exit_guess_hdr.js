9b95df1565df0be1ae2bc5cbf153cdccc0b68509
Guillaume Isabelle <jgi@jgwill.com>
2019-06-11 12:21:19 -0400

    @STCGoal A sequence of shooting contains many HDR.  We read the Exif, observe the BKT -0.7, -1.0 or other data to learn which are sequence //@a We run luminance-hdr-cli -g 1.1 -s ../my_c.tif DSC_1273.NEF  DSC_1274.NEF  DSC_1275.NEF  DSC_1276.NEF  DSC_1277.NEF

---
+++ foexif_read_exit_guess_hdr.js
@@ -0,0 +1,3 @@
+//@STCGoal A sequence of shooting contains many HDR.  We read the Exif, observe the BKT -0.7, -1.0 or other data to learn which are sequence
+//@a We run luminance-hdr-cli -g 1.1 -s ../my_c.tif DSC_1273.NEF  DSC_1274.NEF  DSC_1275.NEF  DSC_1276.NEF  DSC_1277.NEF
+
